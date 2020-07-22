5d1c553a1849fa761c3e747d0d96a9c37899b992
justinro-underscore <jcroderman@gmail.com>
2020-07-22 11:18:55 -0500

    Hooks new name form up to backend

--- web/src/index.js
+++ web/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import {Motion, spring, presets} from 'react-motion';
 import './index.css';
 import FreddyPic from './res/Freddy2.jpg';
+import LoadingSpinner from './res/loading.gif';
 
 function Header(props) {
   return (
@@ -46,7 +47,7 @@ class Poll extends React.Component {
       loading: true,
       rotation: 20
     };
-    this.resetRotationInterval(true);
+    this.rotationInterval = setInterval(() => this.setState({rotation: -20}), 50);
   }
 
   /**
@@ -109,7 +110,7 @@ class Poll extends React.Component {
             <div style={{opacity: style.opacity}}>
               <p className="poll-header">Does this name fit?</p>
               <NameCard name={ this.state.name } rotation={ style.rot }/>
-              <img className="poll-img" src={FreddyPic}></img>
+              <img className="poll-img" src={FreddyPic} alt="Freddy Pic"></img>
               <PollOptions voteFunc={ (voteIsYes) => this.voteOnName(voteIsYes) } rotation={ style.rot }/>
             </div>
           </div>
@@ -123,17 +124,103 @@ class NewNameForm extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      newName: ""
+      newName: "",
+      showInput: true,
+      apiResultText: ""
     };
+    this.loading = false;
+    this.apiResult = null;
+    this.LOADING_WAIT_MILLI_SEC = 1000;
+  }
+
+  sendName(name) {
+    this.setState({showInput: false});
+    this.loading = true;
+    fetch("http://localhost:8080/dognames", {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(name)
+    })
+      .then(
+        (res) => {
+          if (res.status === 200) {
+            res.json().then(
+              (resjson) => {
+                this.apiResult = {
+                  error: false,
+                  body: resjson
+                };
+                this.loading = false;
+              },
+              (error) => this.apiError(error)
+            );
+          }
+          else {
+            res.text().then(
+              (text) => this.apiError(text),
+              (error) => this.apiError(error)
+            );
+          }
+        },
+        (error) => this.apiError(error)
+      );
+  }
+
+  /**
+   * Handles what should happen when there is an error from the API
+   * @param {json} error Describes info from the error
+   */
+  apiError(error) {
+    this.apiResult = {
+      error: true,
+      body: String(error)
+    };
+    this.loading = false;
+    // console.log(error); // TODO Figure out how to show this
   }
 
   handleChange(event) {
-    this.setState({newName: event.target.value});
+    this.setState({
+      newName: event.target.value,
+      apiResultText: ""
+    });
+    this.apiResult = null;
   }
 
   handleSubmit(event) {
-    console.log(this.state.newName);
+    let name = this.state.newName.trim();
+    if (name !== "") {
+      const regexName = new RegExp("^[A-Za-z0-9 ]+$");
+      if (regexName.test(name)) {
+        this.sendName(name);
+        setTimeout(() => this.checkLoading(), this.LOADING_WAIT_MILLI_SEC);
+      }
+      else {
+        this.setState({
+          apiResultText: `Invalid name (name must only include letters, digits, and/or spaces)`
+        });
+        this.apiResult = {
+          error: true,
+          body: ""
+        };
+      }
+    }
     event.preventDefault();
+  }
+
+  checkLoading() {
+    if (!this.loading) {
+      this.setState({
+        newName: "",
+        showInput: true,
+        apiResultText: this.apiResult.error ? this.apiResult.body : `Successfully submitted name "${this.apiResult.body.name}"`
+      });
+    }
+    else {
+      setTimeout(() => this.checkLoading(), this.LOADING_WAIT_MILLI_SEC);
+    }
   }
 
   render() {
@@ -141,11 +228,24 @@ class NewNameForm extends React.Component {
       <div className="new-name-form">
         <form onSubmit={ (event) => this.handleSubmit(event) }>
           <label htmlFor="newName"><h2 className="new-name-form-header">Suggest a name!</h2></label>
-          <div style={{display: "flex"}}>
-            <input type="text" className="new-name-form-input" id="newName" value={ this.state.value }
+          {this.state.showInput ? "" : <img src={LoadingSpinner} className="new-name-form-loading-spinner" />}
+          <div className="new-name-form-input-container" style={{width: this.state.showInput ? "100%" : "0"}}>
+            <input type="text" className="new-name-form-input" id="newName" value={ this.state.newName }
               placeholder="Enter a name..." onChange={ (event) => this.handleChange(event) } />
             <input type="submit" className="new-name-form-submit" value="Submit" />
           </div>
+          {!(this.state.showInput && this.state.apiResultText !== "") ? "" :
+            <Motion
+              defaultStyle={{y: -20}}
+              style={{y: spring(0)}}
+            >
+              {style => (
+                <p style={{transform: `translateY(${style.y}px)`}} className={"new-name-form-api-res" + (this.apiResult.error ? " new-name-form-api-res-error" : "")}>
+                  {this.state.apiResultText}!
+                </p>
+              )}
+            </Motion>
+          }
         </form>
       </div>
     );
