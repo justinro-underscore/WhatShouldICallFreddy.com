04d6fc6ca0e6e6cdb1aa645ebbaab984db2433ad
justinro-underscore <jcroderman@gmail.com>
2020-07-24 16:51:45 -0500

    Adds python script to test

---
+++ voteForNames.py
@@ -0,0 +1,22 @@
+import requests
+import math
+import random
+import time
+
+def vote():
+  base_url = "http://localhost:8080"
+  dog_names = requests.get(base_url + "/dognames").json()
+  dog_names = dog_names["_embedded"]["dogNameList"]
+  dog_name_indeces = list(map(lambda dog : dog["id"], dog_names))
+
+  num_times_to_vote = 50
+  for _ in range(num_times_to_vote):
+    vote_yes = bool(math.floor(random.random() * 2))
+    index = math.floor(random.random() * len(dog_name_indeces))
+    dog_id = dog_name_indeces[index]
+    print("Voting " + ("yes" if vote_yes else "no") + " for id " + str(dog_id))
+    res = requests.post(base_url + "/dognames/vote/" + str(dog_id) + "/" + ("true" if vote_yes else "false"))
+    print("Success!" if res.status_code else "Failure :(")
+    time.sleep(0.2)
+
+vote()
