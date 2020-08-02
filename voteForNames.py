f558604c4137b746f1191887a564ac30121fae51
justinro-underscore <jcroderman@gmail.com>
2020-08-01 20:40:30 -0500

    Updates python script to utilize cookies

--- voteForNames.py
+++ voteForNames.py
@@ -9,13 +9,15 @@ def vote():
   dog_names = dog_names["_embedded"]["dogNameList"]
   dog_name_indeces = list(map(lambda dog : dog["id"], dog_names))
 
+  user = requests.post(base_url + "/user").json()
+
   num_times_to_vote = 50
   for _ in range(num_times_to_vote):
     vote_yes = bool(math.floor(random.random() * 2))
     index = math.floor(random.random() * len(dog_name_indeces))
     dog_id = dog_name_indeces[index]
     print("Voting " + ("yes" if vote_yes else "no") + " for id " + str(dog_id))
-    res = requests.post(base_url + "/dognames/vote/" + str(dog_id) + "/" + ("true" if vote_yes else "false"))
+    res = requests.post(base_url + "/dognames/vote/" + str(dog_id) + "/" + ("true" if vote_yes else "false"), cookies={"user": str(user)})
     print("Success!" if res.status_code else "Failure :(")
     time.sleep(0.2)
 
