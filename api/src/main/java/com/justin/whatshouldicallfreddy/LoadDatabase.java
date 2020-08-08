3a84f38593c06780ad7850caf79e588dfe210c4a
justinro-underscore <jcroderman@gmail.com>
2020-08-08 00:00:36 -0600

    Adds ability to save dog pictures with center position

--- api/src/main/java/com/justin/whatshouldicallfreddy/LoadDatabase.java
+++ api/src/main/java/com/justin/whatshouldicallfreddy/LoadDatabase.java
@@ -1,12 +1,19 @@
 package com.justin.whatshouldicallfreddy;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Arrays;
+import java.util.Scanner;
 
 import com.justin.whatshouldicallfreddy.models.DogName;
 import com.justin.whatshouldicallfreddy.models.DogPicture;
 import com.justin.whatshouldicallfreddy.repos.DogNameRepository;
 import com.justin.whatshouldicallfreddy.repos.DogPictureRepository;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
@@ -17,30 +24,60 @@ import org.springframework.context.annotation.Configuration;
 public class LoadDatabase {
   private static final Logger log = LoggerFactory.getLogger(LoadDatabase.class);
 
-  @Bean
-  CommandLineRunner initDogNameDatabase(DogNameRepository repository) {
-    return args -> {
-      log.info("Preloading " + repository.save(new DogName("Charlie")));
-      log.info("Preloading " + repository.save(new DogName("Max")));
-      log.info("Preloading " + repository.save(new DogName("Buddy")));
-      log.info("Preloading " + repository.save(new DogName("Oscar")));
-      log.info("Preloading " + repository.save(new DogName("Milo")));
-      log.info("Preloading " + repository.save(new DogName("Archie")));
-      log.info("Preloading " + repository.save(new DogName("Ollie")));
-      log.info("Preloading " + repository.save(new DogName("Toby")));
-      log.info("Preloading " + repository.save(new DogName("Jack")));
-      log.info("Preloading " + repository.save(new DogName("Teddy")));
-    };
+  private static void preloadDogNames(DogNameRepository repository) {
+    log.info("Preloading dog names...");
+    String[] names = {"Charlie", "Max", "Buddy", "Oscar", "Milo", "Archie", "Ollie", "Toby", "Jack", "Teddy"}; // Preload top 10 dog names
+    for (String name : names) {
+      log.info("Preloading " + repository.save(new DogName(name)));
+    }
+    log.info("Finished preloading dog names");
+  }
+
+  private static void preloadDogPictures(DogPictureRepository repository) {
+    log.info("Preloading dog pictures...");
+    String imgLocationPath = "api/src/main/resources/image/freddy/";
+    // File[] images = (new File(imgLocationPath)).listFiles();
+    // for (File f : images) {
+    //   log.info("Preloading " + repository.save(new DogPicture("image/freddy/" + f.getName())));
+    // }
+    try {
+      String jsonString = "";
+      Scanner scn = new Scanner(new File(imgLocationPath + "images.json"));
+      while (scn.hasNextLine()) {
+        jsonString += scn.nextLine() + "\n";
+      }
+      scn.close();
+
+      JSONParser parser = new JSONParser();
+      JSONArray dogPictures = (JSONArray)parser.parse(jsonString);
+  
+      for (int i = 0; i < dogPictures.size(); i++) {
+        JSONObject dogPicObj = (JSONObject)dogPictures.get(i);
+        DogPicture dogPicture = new DogPicture("image/freddy/" + dogPicObj.get("fileName"), ((Long)dogPicObj.get("centerX")).intValue(), ((Long)dogPicObj.get("centerY")).intValue());
+        if (dogPicture.getFileName().length() > 0) {
+          log.info("Preloading " + repository.save(dogPicture));
+        }
+        else {
+          log.warn("Could not load dog picture " + dogPicObj.get("fileName") + " (image not found)");
+        }
+      }
+      log.info("Finished preloading dog pictures");
+    }
+    catch (FileNotFoundException e) {
+      log.error("Dog pictures JSON file not found");
+    }
+    catch (ParseException e) {
+      log.error("Dog pictures JSON file could not be parsed");
+    }
   }
 
   @Bean
-  CommandLineRunner initDogPictureDatabase(DogPictureRepository repository) {
+  CommandLineRunner initDogNameDatabase(DogNameRepository dogNameRepo, DogPictureRepository dogPictureRepo) {
     return args -> {
-      String imgLocationPath = "api/src/main/resources/image/freddy/";
-      File[] images = (new File(imgLocationPath)).listFiles();
-      for (File f : images) {
-        log.info("Preloading " + repository.save(new DogPicture("image/freddy/" + f.getName())));
-      }
+      log.info("Initializing API...");
+      preloadDogNames(dogNameRepo);
+      preloadDogPictures(dogPictureRepo);
+      log.info("Completed initialization!");
     };
   }
 }
--- api/src/main/java/com/justin/whatshouldicallfreddy/models/DogPicture.java
+++ api/src/main/java/com/justin/whatshouldicallfreddy/models/DogPicture.java
@@ -1,20 +1,40 @@
 package com.justin.whatshouldicallfreddy.models;
 
+import javax.imageio.ImageIO;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.util.Objects;
 
 @Entity
 public class DogPicture {
   private @Id @GeneratedValue Long id;
   private String fileName;
+  private int normalizedHeight;
+  private int normalizedCenterX;
+  private int normalizedCenterY;
+
+  final private double MAX_WIDTH_PX = 900; // This is hardcoded, discovered from the front end
   
   public DogPicture() {}
 
-  public DogPicture(String fileName) {
-    this.fileName = fileName;
+  public DogPicture(String fileName, int centerX, int centerY) {
+    try {
+      this.fileName = fileName;
+      BufferedImage img = ImageIO.read(new File("api/src/main/resources/" + this.fileName));
+      int width = img.getWidth();
+      int height = img.getHeight();
+
+      double factor = MAX_WIDTH_PX / width;
+      this.normalizedHeight = (int)Math.floor(factor * height);
+      this.normalizedCenterX = (int)Math.floor(factor * centerX);
+      this.normalizedCenterY = (int)Math.floor(factor * centerY);
+    }
+    catch (IOException e) {}
   }
 
   public Long getId() {
@@ -25,12 +45,36 @@ public class DogPicture {
     return this.fileName;
   }
 
+  public int getNormalizedHeight() {
+    return this.normalizedHeight;
+  }
+
+  public int getNormalizeCenterX() {
+    return this.normalizedCenterX;
+  }
+
+  public int getNormalizeCenterY() {
+    return this.normalizedCenterY;
+  }
+
   public void setId(Long id) {
     this.id = id;
   }
 
   public void setFileName(String fileName) {
     this.fileName = fileName;
+  }
+
+  public void setNormalizedHeight(int normalizedHeight) {
+    this.normalizedHeight = normalizedHeight;
+  }
+
+  public void setNormalizedCenterX(int normalizedCenterX) {
+    this.normalizedCenterX = normalizedCenterX;
+  }
+
+  public void setNormalizedCenterY(int normalizedCenterY) {
+    this.normalizedCenterY = normalizedCenterY;
   }
 
   @Override
@@ -40,16 +84,18 @@ public class DogPicture {
     if (!(o instanceof DogPicture))
       return false;
     DogPicture dogPicture = (DogPicture) o;
-    return Objects.equals(this.id, dogPicture.id) && Objects.equals(this.fileName, dogPicture.fileName);
+    return Objects.equals(this.id, dogPicture.id) && Objects.equals(this.fileName, dogPicture.fileName) && Objects.equals(this.normalizedHeight, dogPicture.normalizedHeight)
+      && Objects.equals(this.normalizedCenterX, dogPicture.normalizedCenterX) && Objects.equals(this.normalizedCenterY, dogPicture.normalizedCenterY);
   }
 
   @Override
   public int hashCode() {
-    return Objects.hash(this.id, this.fileName);
+    return Objects.hash(this.id, this.fileName, this.normalizedHeight, this.normalizedCenterX, this.normalizedCenterY);
   }
 
   @Override
   public String toString() {
-    return "DogPicture {" + "id=" + this.id + ", fileName='" + this.fileName + "'}";
+    return "DogPicture {" + "id=" + this.id + ", fileName='" + this.fileName + "', normalizedHeight=" + this.normalizedHeight +
+      ", normalizedCenterX: " + this.normalizedCenterX + ", normalizedCenterY: " + this.normalizedCenterY + "}";
   }
 }
